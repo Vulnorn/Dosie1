fd1341340f7976f6722f68f4f2dd4d9c629fd5ae
Vulnorn <mixadrbk31@gmail.com>
2024-11-25 12:32:43 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Reflection;
 using System.Reflection.Emit;
+using System.Xml.Linq;
 
 namespace Dossier
 {
@@ -16,7 +18,7 @@ namespace Dossier
             int userChoice;
 
             string[] fullNames = new string[0];
-            string[] post = new string[0];  
+            string[] post = new string[0];
 
             while (isWork)
             {
@@ -30,15 +32,15 @@ namespace Dossier
                 switch (userChoice)
                 {
                     case MenuConsoleAddDossier:
-                        AddDossier(fullNames);
+                        AddDossier(fullNames,post);
                         break;
 
                     case MenuConsoleOutputDossier:
-                        ShowAllDossiers(fullNames);
+                        ShowAllDossiers(fullNames, post);
                         break;
 
                     case MenuConsoleDeliteDossier:
-                        DeleteDossierByFullName(fullNames);
+                        DeleteDossierByFullName(fullNames, post);
                         break;
 
                     case MenuConsoleExit:
@@ -52,7 +54,7 @@ namespace Dossier
             }
         }
 
-        static void AddDossier( string [] dossiers)
+        static void AddDossier(string[] fullNames, string[] post)
         {
             string userCreateFullName;
             string userCreatePosition;
@@ -61,7 +63,7 @@ namespace Dossier
             Console.WriteLine($"Введите ФИО Сотрудника:");
             userCreateFullName = Console.ReadLine().ToLower();
 
-            if (dossiers.ContainsKey(userCreateFullName))
+            if (FindElementArray(fullNames, userCreateFullName))
             {
                 ReportAnError("Такой Сотрудник уже есть.");
             }
@@ -70,52 +72,112 @@ namespace Dossier
                 Console.Clear();
                 Console.WriteLine($"Введите должность для этого сотрудника:");
                 userCreatePosition = Console.ReadLine().ToLower();
-                dossiers.Add(userCreateFullName, userCreatePosition);
+
+                ChangeElementArray(fullNames, userCreateFullName);
+                ChangeElementArray(post, userCreatePosition);
             }
         }
 
-        static void ShowAllDossiers(string[] dossiers)
+        static void ShowAllDossiers(string[] fullNames, string[] post)
         {
             Console.Clear();
 
-            if (dossiers.Length == 0)
+            if (fullNames.Length == 0)
             {
                 ReportAnError("Досье пустое, заполните досье!");
             }
             else
             {
-                foreach (var item in dossiers)
+                for (int i = 0; i < fullNames.Length; i++)
                 {
-                    Console.Write($"{item} - {item};");
+                    int numberOrder = i + 1;
+                    Console.WriteLine($"{numberOrder}) {fullNames[i]} - {post[i]}");
                 }
 
                 Console.ReadKey();
             }
         }
 
-        static void DeleteDossierByFullName(string[] dossiers)
+        static void DeleteDossierByFullName(string[] fullNames, string[] post)
         {
             Console.WriteLine($"Введите ФИО сотрудника, которого нужно удалить из досье");
             string userInputWord = Console.ReadLine().ToLower();
+            int index = 0;
 
-            if (dossiers.ContainsKey(userInputWord))
+            if (FindElementArray(fullNames, userInputWord, ref index))
             {
-                dossiers.Remove(userInputWord);
-
-                Console.WriteLine($"Досье удалено.");
-                Console.ReadKey();
+                ChangeElementArray(fullNames,index);
+                ChangeElementArray(post, index);
             }
             else
             {
                 ReportAnError("Нет такого сотрудника");
             }
-
         }
 
         static void ReportAnError(string causeOfError)
         {
             Console.WriteLine($"Ошибка ввода. {causeOfError}");
             Console.ReadKey();
+        }
+
+        static bool FindElementArray(string[] array, string element)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                    return true;
+            }
+
+            return false;
+        }
+
+        static bool FindElementArray(string[] array, string element, ref int index)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static string[] ChangeElementArray(string[] array, string element)
+        {
+            string[] cacheArray = new string[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                cacheArray[i] = array[i];
+            }
+
+            cacheArray[cacheArray.Length] = element;
+            array = cacheArray;
+            return array;
+        }
+
+        static string[] ChangeElementArray(string[] array, int index)
+        {
+            string[] cacheArray = new string[array.Length - 1];
+
+            for (int i = index; i < array.Length - 1; i++)
+            {
+                string bufferElement = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = bufferElement;
+            }
+
+            for (int i = 0; i < cacheArray.Length; i++)
+            {
+                cacheArray[i] = array[i];
+            }
+
+            array = cacheArray;
+            return array;
         }
     }
 }
